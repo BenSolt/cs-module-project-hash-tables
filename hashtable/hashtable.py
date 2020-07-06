1cd85c11e18fe23e8f6fed7e1fc1a4a2bc99ee81
benjamin-solt <bensolt@gmail.com>
2020-07-06 13:37:25 -0400

    starting project

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -23,6 +23,8 @@ class HashTable:
     def __init__(self, capacity):
         # Your code here
 
+        self.capacity = capacity
+        self.storage = [None] * self.capacity
 
     def get_num_slots(self):
         """
